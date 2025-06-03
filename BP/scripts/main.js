3b8c69963c223a11513bbb1902f9aa287f5d90d1
Moelmo <moelmo165@gmail.com>
2025-06-03 08:14:34 +0700

    update fix

--- BP/scripts/main.js
+++ BP/scripts/main.js
@@ -16,9 +16,40 @@ world.beforeEvents.chatSend.subscribe((chatmsg) => {
     const player = chatmsg.sender
     const msg = chatmsg.message
 
+
+    // cek admin bukan
+    // if (!player.hasTag("Admin")) {
+    //     chatmsg.cancel = true
+    //     player.sendMessage("You dont Admin")
+    //     return;
+    // }
+
+    // admin command
+
+    //spesial abilites admin
     if (msg == "!tes") {
-        player.runCommand("say testing")
+        player.runCommand("function gmc")
     }
+
+    if (msg === "!fly") {
+        chatmsg.cancel = true;
+
+        if (player.hasTag("Admin")) {
+            const abilities = player.getComponent("minecraft:abilities");
+
+            if (abilities) {
+                const canFly = abilities.getBoolean("mayfly");
+                abilities.setBoolean("mayfly", !canFly);
+                player.sendMessage(`§aFly ${!canFly ? "enabled" : "disabled"}`);
+            } else {
+                player.sendMessage("§cAbilities component not found. Make sure you enabled GameTest Framework and Beta APIs.");
+            }
+
+        } else {
+            player.sendMessage("§cOnly Admins can use this command.");
+        }
+    }
+
 });
 
 // admin menu
